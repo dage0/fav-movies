f9fbd7e8d03c0f299f4e6ce57e7d8d6015cb0cf2
dage0 <dage@orionsoft.io>
2019-01-24 23:06:35 -0300

    Updates

---
+++ precache-manifest.101c9d0ee6d45c1d023c882616804f9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f0e23fadac38989115c",
+    "url": "/fav-movies/static/js/runtime~main.5f0e23fa.js"
+  },
+  {
+    "revision": "34d645c015185abca6a0",
+    "url": "/fav-movies/static/js/main.34d645c0.chunk.js"
+  },
+  {
+    "revision": "d1edc1533076af42b78b",
+    "url": "/fav-movies/static/js/1.d1edc153.chunk.js"
+  },
+  {
+    "revision": "34d645c015185abca6a0",
+    "url": "/fav-movies/static/css/main.76a00513.chunk.css"
+  },
+  {
+    "revision": "cdd181daedf0c6ce43975c757ba4b767",
+    "url": "/fav-movies/index.html"
+  }
+];
